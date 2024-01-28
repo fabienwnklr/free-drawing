a7ea2ba434a8492deb9f5ab1c86597070d8630e2
Fabien Winkler <fabien.winkler@outlook.fr>
2024-01-29 00:06:51 +0100

    chore: update help dialog and zoom

--- src/components/tools/help/Help.ts
+++ src/components/tools/help/Help.ts
@@ -2,6 +2,7 @@ import './help.scss';
 import { Drawer } from '@/Drawer';
 import { Modal } from '@/components/modal/Modal';
 import HelpIcon from '@/icons/help.svg?raw';
+import ExternalLink from '@/icons/external-link.svg?raw';
 
 export class Help extends Modal {
   $button: HTMLDivElement;
@@ -10,41 +11,57 @@ export class Help extends Modal {
     super(drawer, { title: 'Help' });
 
     const data = /*html*/`
-      <div>
-        <ul>
-          <li>
-          Hand (panning tool) <kbd>H</kbd>
-          </li>
-          <li>
-          Hand (panning tool) <kbd>H</kbd>
-          </li>
-          <li>
-          Hand (panning tool) <kbd>H</kbd>
-          </li>
-          <li>
-          Hand (panning tool) <kbd>H</kbd>
-          </li>
-          <li>
-          Hand (panning tool) <kbd>H</kbd>
-          </li>
-        </ul>
-        <ul>
-        <li>
-        Hand (panning tool) <kbd>H</kbd>
-        </li>
-        <li>
-        Hand (panning tool) <kbd>H</kbd>
-        </li>
-        <li>
-        Hand (panning tool) <kbd>H</kbd>
-        </li>
-        <li>
-        Hand (panning tool) <kbd>H</kbd>
-        </li>
-        <li>
-        Hand (panning tool) <kbd>H</kbd>
-        </li>
-      </ul>
+    <div class="drawer-modal-help-links">
+      <a class="drawer-link" href="">Documentation${ExternalLink}</a>
+      <a class="drawer-link" href="https://github.com/fabienwnklr/free-drawing/" target="_blank">Found issue ? Please inform us !${ExternalLink}</a>
+    </div>
+    <h3>Keyboard shortcuts</h3>
+    <div class="drawer-modal-help-grid">
+      <div class="drawer-modal-help-grid-container">
+        <h4>Tools</h4>
+        <ul class="drawer-modal-help-shortcut-container">
+            <li class="drawer-modal-help-shortcut">
+              <div>
+                Hand (panning tool)
+              </div>
+              <div>
+                <kbd class="drawer-modal-help-kdb">H</kbd>
+              </div>
+            </li>
+            <li class="drawer-modal-help-shortcut">
+            Selection <kbd class="drawer-modal-help-kdb">S</kbd>
+            </li>
+            <li class="drawer-modal-help-shortcut">
+            Brush <kbd class="drawer-modal-help-kdb">B</kbd>
+            </li>
+            <li class="drawer-modal-help-shortcut">
+            Eraser <kbd class="drawer-modal-help-kdb">E</kbd>
+            </li>
+          </ul>
+        </div>
+
+        <div class="drawer-modal-help-grid-container">
+          <h4>Tools</h4>
+          <ul class="drawer-modal-help-shortcut-container">
+            <li class="drawer-modal-help-shortcut">
+              <div>
+                Hand (panning tool)
+              </div>
+              <div>
+                <kbd class="drawer-modal-help-kdb">H</kbd>
+              </div>
+            </li>
+            <li class="drawer-modal-help-shortcut">
+            Selection <kbd class="drawer-modal-help-kdb">S</kbd>
+            </li>
+            <li class="drawer-modal-help-shortcut">
+            Brush <kbd class="drawer-modal-help-kdb">B</kbd>
+            </li>
+            <li class="drawer-modal-help-shortcut">
+            Eraser <kbd class="drawer-modal-help-kdb">E</kbd>
+            </li>
+          </ul>
+        </div>
       </div>
     `
 
@@ -53,7 +70,7 @@ export class Help extends Modal {
     this.$helpContainer = document.createElement('div');
     this.$helpContainer.classList.add('drawer-help-container', 'tool');
     this.$button = document.createElement('div');
-    this.$button.classList.add('button');
+    this.$button.classList.add('drawer-button');
     this.$button.innerHTML = HelpIcon;
 
     this.$helpContainer.append(this.$button);
