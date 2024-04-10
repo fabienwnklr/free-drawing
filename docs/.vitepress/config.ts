31fc1d6e3a4073d78c24a37886476d444dd0f323
Fabienwnklr <49644785+fabienwnklr@users.noreply.github.com>
2024-04-10 16:40:21 +0200

    chore: update giturl

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -43,6 +43,6 @@ export default defineConfig({
       ]
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/fabienwnklr/free-drawing' }],
   },
 });
