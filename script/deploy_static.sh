444559b87cce96aa5bd3854cbefd9c5f0c825a0e
Fabienwnklr <49644785+fabienwnklr@users.noreply.github.com>
2024-04-10 13:57:38 +0200

    chore(ci): add deploy static site script

---
+++ script/deploy_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build:static
+
+# navigate into the build output directory
+cd static
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:fabienwnklr/free-drawing-demo.git master
+
+cd -
