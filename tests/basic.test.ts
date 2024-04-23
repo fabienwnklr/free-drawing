b13e0a3ca7330ffb5a8e8190f207e0f3eb3a05f4
Fabienwnklr <49644785+fabienwnklr@users.noreply.github.com>
2024-04-23 16:47:23 +0200

    test: fix passing test

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -26,7 +26,7 @@ describe('Basic integration', () => {
     expect(drawer.setting).instanceOf(Settings);
     expect(drawer.toolbar.$toolbarContent).instanceOf(HTMLDivElement);
     expect(drawer.toolbar.$toolbarRoot).instanceOf(HTMLDivElement);
-    expect(drawer.toolbar.widgets.size).eq(5);
+    expect(drawer.toolbar.widgets.size).eq(9);
     expect(drawer.activeWidget).not.toBe(null);
     expect(drawer.activeWidget.id).eq('brush');
     expect(drawer.activeWidget.$button.classList.contains('active')).eq(true);
